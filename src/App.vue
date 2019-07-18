21e404d2ed54e49f876cd49cb6aab80854f6a814
Washington Correia <washingtonkle@gmail.com>
2019-07-17 22:13:34 -0300

    Criação de componente para nova tarefa

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
-		<TaskGrid :tasks="tasks"/>
+		<NewTask @taskAdded="addTask" />
+		<TaskGrid :tasks="tasks" @taskDeleted="deleteTask"/>
 	</div>
 </template>
 
 <script>
+import NewTask from './components/NewTask.vue'
 import TaskGrid from './components/TaskGrid.vue'
 
 export default {
-	components: { TaskGrid },
+	components: { NewTask, TaskGrid },
 	data() {
 		return {
-			tasks: [
-				{ name: 'Lavar a louça', pending: false },
-				{ name: 'Comprar blusa', pending:true}
-			]
+			tasks: []
 		}
+	},
+	methods: {
+		addTask( task ) {
+			const sameName = t => t.name === task.name
+			const reallyNew = this.tasks.filter(sameName).length == 0
+			if ( reallyNew ) {
+				this.tasks.push({
+					name: task.name,
+					pending:task.pending || true
+				})
+			}
+ 		},
+		 deleteTask( i ) {
+			 this.tasks.splice( i, 1 )
+		 }
 	}
 }
 </script>
--- src/components/TaskGrid.vue
+++ src/components/TaskGrid.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="task-grid">
         <template v-if="tasks.length">
-            <Task v-for="task in tasks" :key="task.name" :task="task">{{task.name}}</Task>
+            <Task v-for="( task, i ) in tasks" :key="task.name" @taskDeleted="$emit( 'taskDeleted', i )" :task="task">{{ task.name }}</Task>
         </template>
         <p v-else class="no-task">Sua vida está em dia :)</p>
     </div>
